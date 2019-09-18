9a9cbb3523561b0ca8f145bb2a7ee770c2071cac
Thomas Ehmann <ehmannth@gmail.com>
2019-09-18 17:21:13 +0200

    Added Services section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,15 +2,22 @@ import React from "react";
 import Hero from "../components/Hero";
 import Banner from "../components/Banner";
 import { Link } from "react-router-dom";
+import Services from "../components/Services";
 
 export default function Home() {
   return (
-    <Hero hero="defaultHero">
-      <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
-        <Link to="/rooms" className="btn btn-primary">
-          our rooms
-        </Link>
-      </Banner>
-    </Hero>
+    <>
+      <Hero hero="defaultHero">
+        <Banner
+          title="luxurious rooms"
+          subtitle="deluxe rooms starting at $299"
+        >
+          <Link to="/rooms" className="btn btn-primary">
+            our rooms
+          </Link>
+        </Banner>
+      </Hero>
+      <Services></Services>
+    </>
   );
 }
