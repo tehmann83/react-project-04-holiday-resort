08900593c1b927a0025b990c05ff9774c11d6145
Thomas Ehmann <ehmannth@gmail.com>
2019-09-18 13:00:42 +0200

    Added Navbar component

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import Home from "./pages/Home";
 import Rooms from "./pages/Rooms";
 import SingleRoom from "./pages/SingleRoom";
 import Error from "./pages/Error";
+import Navbar from "./components/Navbar";
 
 import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Switch>        
+      <Navbar></Navbar>
+      <Switch>
         <Route path="/" exact component={Home}></Route>
         <Route path="/rooms/" exact component={Rooms}></Route>
         <Route path="/rooms/:id" exact component={SingleRoom}></Route>
