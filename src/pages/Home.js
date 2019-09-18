7e2e4ce9b8ffc47c44ee8e3e6bdd059353ca2231
Thomas Ehmann <ehmannth@gmail.com>
2019-09-18 13:34:36 +0200

    Added Banner component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,16 @@
-import React from 'react'
-import Hero from "../components/Hero"
+import React from "react";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <Hero hero="defaultHero"></Hero>
-  )
+    <Hero hero="defaultHero">
+      <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
+        <Link to="/rooms" className="btn btn-primary">
+          our rooms
+        </Link>
+      </Banner>
+    </Hero>
+  );
 }
