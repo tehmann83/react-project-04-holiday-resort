802e92896470ffe9847e1a28ef6aa5cbd9b22d6a
Thomas Ehmann <ehmannth@gmail.com>
2019-09-18 12:31:58 +0200

    Added Router setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+import Error from "./pages/Error";
+
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <>
-    my app
+      <Switch>        
+        <Route path="/" exact component={Home}></Route>
+        <Route path="/rooms/" exact component={Rooms}></Route>
+        <Route path="/rooms/:id" exact component={SingleRoom}></Route>
+        <Route component={Error}></Route>
+      </Switch>
     </>
   );
 }
