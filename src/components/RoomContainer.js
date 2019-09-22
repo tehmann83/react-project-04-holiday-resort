4ecd856f97b7151c9258dc851784d61796c073ca
Thomas Ehmann <ehmannth@gmail.com>
2019-09-22 15:24:07 +0200

    Added RoomContainer

---
+++ src/components/RoomContainer.js
@@ -0,0 +1,41 @@
+import React from "react";
+import RoomFilter from "./RoomFilter";
+import RoomList from "./RoomList";
+import { withRoomConsumer } from "../Context";
+import Loading from "./Loading";
+
+function RoomContainer({ context }) {
+  const { loading, sortedRooms, rooms } = context;
+  if (loading) {
+    return <Loading />;
+  }
+  return (
+    <div>
+      RoomContainer
+      <RoomFilter rooms={rooms} />
+      <RoomList rooms={sortedRooms} />
+    </div>
+  );
+}
+
+export default withRoomConsumer(RoomContainer);
+
+// export default function RoomsContainer() {
+//   return (
+//     <RoomConsumer>
+//       {value => {
+//         const { loading, sortedRooms, rooms } = value;
+//         if (loading) {
+//           return <Loading />;
+//         }
+//         return (
+//           <div>
+//             RoomContainer
+//             <RoomFilter rooms={rooms} />
+//             <RoomList rooms={sortedRooms} />
+//           </div>
+//         );
+//       }}
+//     </RoomConsumer>
+//   );
+// }
--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -2,9 +2,11 @@ import React from "react";
 import Hero from "../components/Hero";
 import Banner from "../components/Banner";
 import { Link } from "react-router-dom";
+import RoomContainer from "../components/RoomContainer";
 
 export default function Rooms() {
   return (
+    <>
     <Hero hero="roomsHero">
       <Banner title="our rooms">
         <Link to="/" className="btn btn-primary">
@@ -12,5 +14,7 @@ export default function Rooms() {
         </Link>
       </Banner>
     </Hero>
+    <RoomContainer/>
+    </>
   );
 }
